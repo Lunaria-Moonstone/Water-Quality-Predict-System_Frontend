29bf08badb66b86ef77c96b4b402aa82c0d711f9
Sill <moonstone.lunaria@gmail.com>
2023-12-10 12:06:40 +0800

    build sign page

--- src/app/(pages)/(authorized)/sign/page.tsx
+++ src/app/(pages)/(authorized)/sign/page.tsx
@@ -6,7 +6,37 @@ function Sign() {
 			<div className="back"></div>
 			{/* <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">sign in</button> */}
 			<div className="sign-box">
-
+				<div>
+					<div className="sign-box-title">Authorized</div>
+				</div>
+				<div className="sign-box-lines">
+					<div className="sign-box-line relative h-11 w-full min-w-[200px]">
+						<input
+							className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+							placeholder=" " />
+						<label
+							className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+							Username
+						</label>
+					</div>
+					<div className="sign-box-line relative h-11 w-full min-w-[200px]">
+						<input
+							className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+							placeholder=" " type="password" />
+						<label
+							className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+							Password
+						</label>
+					</div>
+				</div>
+				<div className="signin-button">
+					<button
+						className="select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+						type="button"
+					>
+						Sign in
+					</button>
+				</div>
 			</div>
 		</div>
 	)
