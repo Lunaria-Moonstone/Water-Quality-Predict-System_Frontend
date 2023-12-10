613158d70381a1ac3cb042b2bede89307002d1fc
Lunaria-Moonstone <moonstone.lunaria@gmail.com>
2023-12-10 10:57:06 +0800

    start to build login

---
+++ src/app/(pages)/(authorized)/sign/page.tsx
@@ -0,0 +1,15 @@
+import "@/assets/pagestyle/athorized.css"
+
+function Sign() {
+	return (
+		<div>
+			<div className="back"></div>
+			{/* <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">sign in</button> */}
+			<div className="sign-box">
+
+			</div>
+		</div>
+	)
+}
+
+export default Sign
